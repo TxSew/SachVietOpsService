40d3cae5925003f61df504fb4c2dcc4f3b56efd1
tomnyson <tabletkindfire@gmail.com>
2023-10-23 22:42:28 +0700

    update code cicd

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+cd /var/www/thanh
+npm install -g @nestjs/cli
+npm install -g pm2
+
+git pull orgin master
+cp  .env.dev .env
+npm install
+npm run build
+pm2 start dist/main.js --name thanh-api
+pm2 reload thanh-api
+exit 0
