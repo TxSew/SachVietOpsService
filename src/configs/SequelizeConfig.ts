d301d078b7b062a1ee93d8b4976308eb6beb3ab6
tomnyson <tabletkindfire@gmail.com>
2023-11-07 21:42:12 +0700

    update code

--- src/configs/SequelizeConfig.ts
+++ src/configs/SequelizeConfig.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 dotenv.config();
 
 const {DB_USERNAME, DB_PASSWORD, DB_NAME} = process.env
-export const SequelizeBase = new Sequelize('db_books', process.env.DB_USERNAME, '', {
+export const SequelizeBase = new Sequelize(DB_NAME || 'db_books', DB_USERNAME || 'root', DB_PASSWORD || '', {
   host: 'localhost',
   dialect: 'mysql',
 });
