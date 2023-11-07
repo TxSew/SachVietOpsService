7c35bd940dba51f589b94f89825af64dbadf1b06
tomnyson <tabletkindfire@gmail.com>
2023-11-07 21:29:58 +0700

    remove .env

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
-cd /var/www/thanh
-npm install -g @nestjs/cli
-npm install -g pm2
-git checkout -- .
-git pull orgin main
-# cp  .env.dev .env
+# cd /var/www/thanh
+# npm install -g @nestjs/cli
+# npm install -g pm2
+git checkout --
+git pull origin main
+cp  .env.prod .env
 npm install
 npm run build
 pm2 start dist/main.js --name thanh-api
