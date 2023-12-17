cc77d09303ae4a7fd961a429dfc1d30d12b6eb63
tomnyson <tabletkindfire@gmail.com>
2023-12-17 13:48:53 +0700

    update code mysql connect

--- src/configs/SequelizeConfig.ts
+++ src/configs/SequelizeConfig.ts
@@ -2,8 +2,8 @@ import { Sequelize } from 'sequelize';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const {DB_USERNAME, DB_PASSWORD, DB_NAME} = process.env
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST } = process.env;
 export const SequelizeBase = new Sequelize(DB_NAME || 'db_books', DB_USERNAME || 'root', DB_PASSWORD || '', {
-  host: 'localhost',
-  dialect: 'mysql',
+    host: DB_HOST || 'localhost',
+    dialect: 'mysql',
 });
